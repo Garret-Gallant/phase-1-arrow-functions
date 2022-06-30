3231c2f9c44f2e66c2533e7e594dbd3b0697d7d4
Garret Gallant <gallanttechnological@gmail.com>
2022-06-30 09:11:04 -0600

    commit tree 76c6a8ebc56579c41d9fd3f772629ab9da56f519

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide() {
+    let a = 2000
+    let b = 100
+    let result = 0;
+    result = a / b;
+    return result;
+}
+
+const square = (num1) => num1 * num1;
+
+const add = (num1, num2) => num1 + num2;
